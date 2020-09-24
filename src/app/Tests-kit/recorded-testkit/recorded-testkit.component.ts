b40aae786811663d5cf4804a9a2906236b1f67c9
william <williamtjaja@users.noreply.github.com>
2020-09-24 15:24:08 +0700

    interface only

--- src/app/Tests-kit/recorded-testkit/recorded-testkit.component.ts
+++ src/app/Tests-kit/recorded-testkit/recorded-testkit.component.ts
@@ -1,21 +1,38 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
+
 
 @Component({
   selector: 'app-main-testkit',
   templateUrl: './recorded-testkit.component.html',
   styleUrls: ['./recorded-testkit.component.css']
 })
-export class RecordedTestkitComponent {
+export class RecordedTestkitComponent implements AfterViewInit{
+  displayedColumns: string[] = ['kitID', 'name', 'stock', 'edit'];
+  dataSource = new MatTableDataSource<testKit>(KIT_DATA);
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
-
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
+
+export interface testKit {
+  name: string;
+  kitID: number;
+  stock: number;
+  edit: string;
+}
+
+const KIT_DATA: testKit[] = [
+  {kitID: 1, name: 'Hydrogen', stock: 10, edit: ''},
+  {kitID: 2, name: 'Helium', stock: 100, edit: ''},
+  {kitID: 3, name: 'Lithium', stock: 200, edit: ''},
+  {kitID: 4, name: 'Beryllium', stock: 9, edit: ''},
+  {kitID: 5, name: 'Boron', stock: 11, edit: ''},
+];
+
+
